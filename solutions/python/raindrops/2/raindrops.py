156b923f5e77e0a632b88c64832474b5bd5a1006
root <root@ip-10-1-2-30.eu-west-2.compute.internal>
2025-06-06 04:40:59 +0000

    [Sync Iteration] python/raindrops/2

---
+++ solutions/python/raindrops/2/raindrops.py
@@ -0,0 +1,34 @@
+"""
+This module provides a function to convert a number into a "Raindrop" sound.
+
+The conversion follows specific rules:
+- "Pling" if the number is divisible by 3.
+- "Plang" if the number is divisible by 5.
+- "Plong" if the number is divisible by 7.
+- If not divisible by 3, 5, or 7, the number itself is returned as a string.
+"""
+
+def convert(number: int) -> str:
+    """
+    Converts a given integer into its "Raindrop" sound representation.
+
+    Args:
+        number (int): The integer to convert.
+
+    Returns:
+        str: A string representing the "Raindrop" sound. This will be a
+             concatenation of "Pling", "Plang", and/or "Plong" based on
+             divisibility by 3, 5, and 7 respectively. If the number is not
+             divisible by any of these, the number itself is returned as a string.
+    """
+    result = ""
+    if number % 3 == 0:
+        result += "Pling"
+    if number % 5 == 0:
+        result += "Plang"
+    if number % 7 == 0:
+        result += "Plong"
+    if not result:
+        return str(number)
+
+    return result
